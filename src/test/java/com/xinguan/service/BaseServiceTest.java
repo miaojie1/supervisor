790e87c2fa514c16651f5148329facce1bd287c6
zhangzhan <zhan.zhang@realhead.cn>
2019-03-24 21:46:42 +0800

    添加单元测试

---
+++ src/test/java/com/xinguan/service/BaseServiceTest.java
@@ -0,0 +1,22 @@
+package com.xinguan.service;
+
+import com.xinguan.Application;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * @author zhangzhan
+ * @date 2019-03-24 21:44
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+public class BaseServiceTest extends AbstractTestNGSpringContextTests {
+    @Autowired
+    protected EmployeeService employeeService;
+
+}
---
+++ src/test/java/com/xinguan/service/impl/EmployeeServiceImplTest.java
@@ -0,0 +1,45 @@
+package com.xinguan.service.impl;
+
+import com.xinguan.model.Employee;
+import com.xinguan.service.BaseServiceTest;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertNull;
+
+
+public class EmployeeServiceImplTest extends BaseServiceTest {
+
+
+    @BeforeMethod
+    public void setUp() {
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+    }
+
+    @Test
+    public void testFindEmployeeByUsernameAndPassword() {
+
+    }
+
+
+    @Test(dataProvider = "usernameParam")
+    public void testFindEmployeeByUsername(String username) {
+        Employee employee = employeeService.findEmployeeByUsername(username);
+        assertNull(employee);
+    }
+
+    @Test
+    public void testGetCurrentUser() {
+    }
+
+    @DataProvider(name = "usernameParam")
+    private Object[] getParamters() {
+        return new String[]{null, "", "admin", "xinguan"};
+    }
+}
